6f17d4473f26a039bfda11566546ccdb032064f1
sddale <seth.d.dale@gmail.com>
2026-03-03 18:57:15 -0700

    positional embeddings

--- src/transformer/transformer.py
+++ src/transformer/transformer.py
@@ -10,15 +10,15 @@ class Transformer(nn.Module):
     def __init__(self, conf: Config, device=torch.device("cpu")) -> None:
         super().__init__()
         self.embed = torch.nn.Embedding(conf.d_vocab, conf.d_model, device=device)
+        self.pos_embed = torch.nn.Embedding(conf.n_context, conf.d_model, device=device)
         self.blocks = nn.ModuleList(
-            [Block(conf).to(device) for i in range(conf.n_blocks)]
+            [Block(conf).to(device) for _ in range(conf.n_blocks)]
         )
 
         self.lm_head = nn.Linear(conf.d_model, conf.d_vocab, bias=False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        out = self.embed(x)
-        # print(f"{out.shape=}")
+        out = self.embed(x) + self.pos_embed(torch.arange(x.size(1), device=x.device))
         for block in self.blocks:
             out = block(out)
         out = self.lm_head(out)
